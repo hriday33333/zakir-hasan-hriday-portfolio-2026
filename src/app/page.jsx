4431ec0b7e2f8ef5e154f0c2b19a1b8c0a1f9177
hriday33333 <zakirhasanhriday@gmail.com>
2026-07-31 14:25:35 +0600

    add herosection

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,10 +1,14 @@
 import HeaderSection from "@/components/HeaderSection";
+import Navbar from "@/components/Navbar";
 import Image from "next/image";
 
 export default function Home() {
   return (
-      <main className="flex min-h-screen flex-col bg-[#121212] mx-auto px-12 py-4">
-       <HeaderSection></HeaderSection>
+      <main className="flex min-h-screen flex-col bg-[#121212] ">
+      <Navbar></Navbar>
+       <div ClassName="container mx-auto px-12 py-4">
+        <HeaderSection></HeaderSection>
+       </div>
       </main>
   );
 }
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,29 @@
+import Link from 'next/link';
+
+const Navbar = () => {
+  return (
+    <nav>
+      <div className="flex flex-wrap items-center justify-between mx-auto p-8">
+        <Link href={'/'} className="text-5xl text-white font-semibold">
+          LOGO
+        </Link>
+        <div className="menu">
+          <ul>
+            <li>
+              <Link href={'#about'} className="">
+                About
+              </Link>
+            </li>
+            <li>
+              <Link href={'#contact'} className="">
+                Contact
+              </Link>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </nav>
+  );
+};
+
+export default Navbar;
