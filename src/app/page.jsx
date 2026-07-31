c4c3a08d6e291b0eb5fff6f9a083e9f68a0bf286
hriday33333 <zakirhasanhriday@gmail.com>
2026-07-31 10:41:12 +0600

    add my first commit

---
+++ src/app/page.jsx
@@ -0,0 +1,10 @@
+import HeaderSection from "@/components/HeaderSection";
+import Image from "next/image";
+
+export default function Home() {
+  return (
+      <main className="flex min-h-screen flex-col bg-[#121212] mx-auto px-12 py-4">
+       <HeaderSection></HeaderSection>
+      </main>
+  );
+}
---
+++ src/components/HeaderSection.jsx
@@ -0,0 +1,36 @@
+import React from 'react';
+import Image from 'next/image'
+
+const HeaderSection = () => {
+  return (
+    <section>
+      <div className="grid grid-cols-1 lg:grid-cols-12">
+      <div className="col-span-7 place-self-center">
+        <h1 className="text-white mb-4 text-4xl lg:text-6xl font-extrabold">Hello, Iam Heiday</h1>
+      <p className="text-[#ADB7BE] text-lg mb-6 lg:text-xl">Welcome to my portfolio!</p>
+      <div>
+        <button className="px-6 py-3 rounded-full mr-4 bg-white hover:bg-slate-200 text-black">
+          Hire Me
+        </button>
+        <button className="px-6 py-3 rounded-full bg-transparent hover:bg-slate-800 text-white border border-white">
+          download Resume
+        </button>
+       </div>
+      </div>
+      <div className="col-span-5 place-self-center mt-4 lg:mt-0">
+      <div className="rounded-full bg-[#181818] w-[250px] h-[250px] lg:w-[400px] lg:h-[400px] relative">
+        <Image
+          src="/images/h-image.png"
+          alt="Profile Image"
+          className="absolute transform -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2 rounded-full"
+          width={300}
+          height={300}
+        />
+      </div>
+      </div>
+      </div>
+    </section>
+  );
+};
+
+export default HeaderSection;
